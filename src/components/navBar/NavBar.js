a6f24ef637061e37012bba3604dd1e8bcb8c728e
Juan Amoros <juaniamoros2001@gmail.com>
2022-08-29 19:05:23 -0300

    agregonavbar

---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,20 @@
+
+export default function NavBar(){
+    return <nav className="navbar"> 
+    <a href="" className="titulo">Tienda Virtual</a>
+    <ul> <a href="" className="primeraOpcion">Primera</a></ul>
+    <ul> <a href="" className="segundaOpcion">Segunda</a></ul>
+    <ul> <a href="" className="terceraOpcion">Tercera</a></ul>
+    </nav>
+} 
+
+
+
+/* 
+const NavBar = () => {
+    return ( <div>hola 
+    </div>)
+};
+ */
+
+/* export default NavBar; */
